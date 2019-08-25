3cc96e99fb0575293dc780286f0fb9aa5e0567aa
Keith Isham <kisham8899@gmail.com>
2019-08-25 16:13:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,48 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  # get '/goodbye/:name' do
+  #   @user_name = params[:name]
+  #   "Goodbye, #{@user_name}."
+  # end
+
+  # get '/multiply/:num1/:num2' do
+  #   @num1 = params[:num1].to_i
+  #   @num2 = params[:num2].to_i
+  #   "#{@num1 * @num2}"
+  # end
+  
+  # ^^ previous examples. 
+  
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "#{@user_name}".reverse 
+  end
+  
+  get '/square/:number' do
+    @num = params[:number].to_i
+    
+   erb :square
+    
+  end
+  
+  get '/say/:number/:phrase' do 
+  @number = params[:number].to_i
+  @phrase = params[:phrase]
+  
+  erb :say
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  @w1 = params[:word1]
+  @w2 = params[:word2]
+  @w3 = params[:word3]
+  @w4 = params[:word4]
+  @w5 = params[:word5]
+  "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}."
+  
+  
+  # <%= "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}." %>
+end 
 
 end
