30521dd1f296a1bc1e7fe046aac5837892890660
Keith Isham <kisham8899@gmail.com>
2019-08-25 16:17:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -42,7 +42,14 @@ class App < Sinatra::Base
   "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}."
   
   
-  
+  get '/:operation/:number1/:number2' do 
+    @number1 = params[:number1]
+    @number2 = params[:number2] 
+    
+    
+    
+    
+  end
 end 
 
 end
